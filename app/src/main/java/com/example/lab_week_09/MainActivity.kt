158c79b804735b822a8bea206508d28c2c7b6668
YaredMordechai <165069915+YaredMordechai@users.noreply.github.com>
2025-11-03 16:29:57 +0700

    Commit No. 2 – State and Event Handler

--- app/src/main/java/com/example/lab_week_09/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_09/MainActivity.kt
@@ -8,70 +8,171 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.lab_week_09.ui.theme.LAB_WEEK_09Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
+
             LAB_WEEK_09Theme {
+
                 Surface(
+
                     modifier = Modifier.fillMaxSize(),
+
                     color = MaterialTheme.colorScheme.background
+
                 ) {
-                    val list = listOf("Tanu", "Tina", "Tono")
-                    Home(list) // panggil composable sesuai modul
+
+                    Home() // panggil composable sesuai modul
+
                 }
+
             }
+
         }
+
     }
+
 }
 
+
+
+data class Student(
+
+    var name: String
+
+)
+
+
+
 @Composable
-fun Home(
-    items: List<String>,
+
+fun Home() {
+
+    val listData = remember {
+
+        mutableStateListOf(
+
+            Student("Tanu"),
+
+            Student("Tina"),
+
+            Student("Tono")
+
+        )
+
+    }
+
+    val inputField = remember { mutableStateOf(Student("")) }
+
+
+
+    HomeContent(
+
+        listData = listData,
+
+        inputField = inputField.value,
+
+        onInputValueChange = { input ->
+
+            inputField.value = inputField.value.copy(input)
+
+        },
+
+        onButtonClick = {
+
+            if (inputField.value.name.isNotBlank()) {
+
+                listData.add(inputField.value)
+
+                inputField.value = Student("")
+
+            }
+
+        }
+
+    )
+
+}
+
+
+
+@Composable
+
+fun HomeContent(
+
+    listData: SnapshotStateList<Student>,
+
+    inputField: Student,
+
+    onInputValueChange: (String) -> Unit,
+
+    onButtonClick: () -> Unit
+
 ) {
+
     LazyColumn {
+
         item {
+
             Column(
+
                 modifier = Modifier
+
                     .padding(16.dp)
+
                     .fillMaxSize(),
+
                 horizontalAlignment = Alignment.CenterHorizontally
+
             ) {
+
                 Text(text = stringResource(id = R.string.enter_item))
+
+
+
                 TextField(
-                    value = "",
-                    onValueChange = { },
-                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+
+                    value = inputField.name,
+
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
+
+                    onValueChange = { onInputValueChange(it) }
+
                 )
-                Button(onClick = { }) {
+
+
+
+                Button(onClick = { onButtonClick() }) {
+
                     Text(text = stringResource(id = R.string.button_click))
                 }
             }
         }
-        items(items) { item ->
+
+
+
+        items(listData) { item ->
+
             Column(
                 modifier = Modifier
                     .padding(vertical = 4.dp)
                     .fillMaxSize(),
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                Text(text = item)
+                Text(text = item.name)
+
             }
         }
     }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun PreviewHome() {
-    Home(listOf("Tanu", "Tina", "Tono"))
 }
